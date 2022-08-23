388c14eef55b73dee0c73f84a61ba6000433fa57
hgyuhyeon <hwgyuhyeon@gmail.com>
2022-08-23 12:15:16 +0900

    feat: 타임머신 파일 생성

---
+++ src/api/capsule.py
@@ -0,0 +1,21 @@
+from flask_restx import Resource, Namespace
+
+Capsule = Namespace('Capsule')
+
+@Capsule.route('/')
+class CapsuleMainAPI(Resource):
+    def get(self):
+        pass # read
+
+@Capsule.route('/create')
+class CapsuleCreateAPI(Resource):
+    def post(self):
+        pass # create
+
+@Capsule.route('/<int:id>')
+class CapsuleAPI(Resource):
+    def put(self):
+        pass # update
+
+    def delete(self):
+        pass # delete
--- src/app.py
+++ src/app.py
@@ -1,13 +1,22 @@
-from flask import flask
-from flask_restx import Resource, Api
-from api.main import main
+from flask import Flask
+from flask_restx import Api
+from api.main import Main
+from api.capsule import Capsule
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(
+            app,
+            version='0.1',
+            title="Hoee API Server",
+            description="Hoee blog API Server",
+            terms_url="/",
+            contact="hwgyuhyeon@gmail.com",
+)
 
     
-api.add_namespace(Main, '/')
+api.add_namespace(Main, '')
+api.add_namespace(Capsule, '/capsule')
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=80, debug=True)
