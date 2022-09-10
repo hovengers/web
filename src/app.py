4cb134df95579c1bb989aca31c89e55b70111a6e
hgyuhyeon <hwgyuhyeon@gmail.com>
2022-09-10 22:20:09 +0900

    db 연결방식 변경

--- src/app.py
+++ src/app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask_restx import Api
+from flask_sqlalchemy import SQLAlchemy
+import dbconfig
+
 from api.main import Main
 from api.capsule import Capsule
 
@@ -9,14 +12,19 @@ api = Api(
             version='0.1',
             title="Hoee API Server",
             description="Hoee blog API Server",
-            terms_url="/",
+            terms_url="/api",
             contact="hwgyuhyeon@gmail.com",
 )
 
-    
-api.add_namespace(Main, '')
-api.add_namespace(Capsule, '/capsule')
+# api
+api.register_blueprint(Main)
+api.register_blueprint(Capsule)
 
+# db 
+app.config["SQLALCHEMY_DATABASE_URI"] = dbconfig.DB_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy()
+db.init_app(app)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=80, debug=True)
