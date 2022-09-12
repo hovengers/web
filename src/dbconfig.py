99c7d40eed0e616cc94114fea98cee612b6eb959
hgyuhyeon <hwgyuhyeon@gmail.com>
2022-09-12 14:33:31 +0900

    gitignore 변경

--- src/dbconfig.py
+++
@@ -1,9 +0,0 @@
-db = {
-    'user': 'root',
-    'password': '1234',
-    'host': 'localhost',
-    'port': 3306,
-    'database': 'hoee'
-}
-
-DB_URI = f"mysql+pymysql://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}"
