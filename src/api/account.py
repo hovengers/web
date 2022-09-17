496582512a9f41f3f9de4c6b371f6bbc6bc76886
hgyuhyeon <hwgyuhyeon@gmail.com>
2022-09-18 00:14:23 +0900

    비밀번호 해싱 추가

--- src/api/account.py
+++ src/api/account.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, request, render_template, jsonify, session, redirect, url_for
-from models import user_model as db_user
+from sqlite3 import DatabaseError
+from flask import current_app, Blueprint, request, render_template, jsonify, session, redirect, url_for
+import models
 from flask_sqlalchemy import SQLAlchemy
+import bcrypt
 
 db = SQLAlchemy()
 Account = Blueprint('Account', __name__)
@@ -8,24 +10,22 @@ Account = Blueprint('Account', __name__)
 @Account.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method =='POST':
-        new_user = db_user.User(id=request.form.get('id'), password=request.form.get('pw'), name=request.form.get('name'))
+        hashpw = bcrypt.hashpw(request.form.get('pw').encode("utf-8"), bcrypt.gensalt())
+        print(bcrypt.checkpw(request.form.get('pw').encode("utf-8"), hashpw))
+        new_user = models.User(id=request.form.get('id'), password=hashpw, name=request.form.get('name'))
         db.session.add(new_user)
         db.session.commit()
-        return redirect(url_for('signin'))
+        return redirect(url_for('Account.signin'))
     else:
         return render_template('account/signup.html')
-# 비밀번호 암호화때문에 회원가입 페이지를 만들어두지만
-# 별도 회원가입 없이 이 페이지로 친구들 계정 미리 생성해주기
-# 아이디는 실명, 초기 비밀번호는...
 
 @Account.route('/signin', methods=['GET', 'POST'])
 def signin():
     if request.method =='POST':
-        id = request.form.get('id')
-        pw = request.form.get('pw')
         try:
-            data = db_user.User.query.filter_by(id=id, password=pw).first()
-            if data:
+            data = models.User.query.filter_by(id=request.form.get('id')).first()
+            print(data.password, ", ", bcrypt.checkpw(request.form.get('pw').encode('utf-8'), data.password.encode('utf-8')))
+            if data and bcrypt.checkpw(request.form.get('pw').encode('utf-8'), data.password.encode('utf-8')):
                 session['userid'] = data.id
                 session['username'] = data.name
                 return redirect(url_for('Main.main'))
--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
 import config
@@ -6,6 +7,7 @@ import config
 from api.main import Main
 from api.account import Account
 from api.capsule import Capsule
+from api.kakaotalk import Kakaotalk
 
 app = Flask(__name__)
 # api = Api(
@@ -21,6 +23,7 @@ app = Flask(__name__)
 app.register_blueprint(Main)
 app.register_blueprint(Account)
 app.register_blueprint(Capsule)
+app.register_blueprint(Kakaotalk)
 
 # db 
 app.config["SQLALCHEMY_DATABASE_URI"] = config.DB_URI
@@ -34,6 +37,9 @@ app.config['JSON_AS_ASCII'] = False
 
 # key
 app.secret_key = config.SECRET_KEY
+app.config['BCRYPT_LEVEL'] = 10
+
+bcrypt = Bcrypt(app)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=80, debug=True)
